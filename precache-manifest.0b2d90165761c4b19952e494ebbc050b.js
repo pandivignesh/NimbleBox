46e0614c4506631cba9109034f06de037581f7d7
pandivigneshA <pandi.v@askpundit.com>
2020-07-26 22:12:40 +0530

    Updates

---
+++ precache-manifest.0b2d90165761c4b19952e494ebbc050b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61046232f653e8cac6f7f29cf29b1ffa",
+    "url": "/NimbleBox/index.html"
+  },
+  {
+    "revision": "4ef8b3006b0e26b15fdc",
+    "url": "/NimbleBox/static/css/main.3d9a5e0f.chunk.css"
+  },
+  {
+    "revision": "602f395ee5ee2061cef1",
+    "url": "/NimbleBox/static/js/2.2ebd7e81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NimbleBox/static/js/2.2ebd7e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6dbcb657ad30faac7d",
+    "url": "/NimbleBox/static/js/3.ae0d2c86.chunk.js"
+  },
+  {
+    "revision": "634619e875b832e51ba4",
+    "url": "/NimbleBox/static/js/4.e29946b3.chunk.js"
+  },
+  {
+    "revision": "c99211d05553ab84dc963e3d001d0eaa",
+    "url": "/NimbleBox/static/js/4.e29946b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3683d10ef1dc4561ad",
+    "url": "/NimbleBox/static/js/5.273cda2b.chunk.js"
+  },
+  {
+    "revision": "2e5c0044ead56d09042a",
+    "url": "/NimbleBox/static/js/6.1ace7400.chunk.js"
+  },
+  {
+    "revision": "35f95e9b01ddaf235c91",
+    "url": "/NimbleBox/static/js/7.3282f7e3.chunk.js"
+  },
+  {
+    "revision": "4ef8b3006b0e26b15fdc",
+    "url": "/NimbleBox/static/js/main.9bf30fb7.chunk.js"
+  },
+  {
+    "revision": "ef0baf2786ff77a5dbf6",
+    "url": "/NimbleBox/static/js/runtime-main.9282ab86.js"
+  },
+  {
+    "revision": "2ad7bf035eba013c51e46396b473e6df",
+    "url": "/NimbleBox/static/media/dls-icon-cancel-circle-filled.2ad7bf03.svg"
+  },
+  {
+    "revision": "4f8b1bf6a8f603c18d5793db78f0e218",
+    "url": "/NimbleBox/static/media/dls-icon-success-filled.4f8b1bf6.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/NimbleBox/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/NimbleBox/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/NimbleBox/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/NimbleBox/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/NimbleBox/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "83dde36c448f07a945e50ae46b619d27",
+    "url": "/NimbleBox/static/media/loader.83dde36c.gif"
+  },
+  {
+    "revision": "dc61d26c8a7ca0a5e5ca740ec08a0614",
+    "url": "/NimbleBox/static/media/logo.dc61d26c.png"
+  }
+]);
